919e631ed2e429bc33ea99dd471c8c8d4701f97e
Mustafa Barez <mustafabarez@hotmail.com>
2020-03-10 15:37:23 -0400

    Add files via upload

---
+++ K-Means-Scala/SparkKMeans.scala
@@ -0,0 +1,79 @@
+
+import breeze.linalg.{squaredDistance, DenseVector, Vector}
+
+import org.apache.spark.sql.SparkSession
+
+/**
+ * K-means clustering using Scala
+ */
+object SparkKMeans {
+
+  def parseVector(line: String): Vector[Double] = {
+    DenseVector(line.split(' ').map(_.toDouble))
+  }
+
+  def closestPoint(p: Vector[Double], centers: Array[Vector[Double]]): Int = {
+    var bestIndex = 0
+    var closest = Double.PositiveInfinity
+
+    for (i <- 0 until centers.length) {
+      val tempDist = squaredDistance(p, centers(i))
+      if (tempDist < closest) {
+        closest = tempDist
+        bestIndex = i
+      }
+    }
+
+    bestIndex
+  }
+
+  def showWarning(): Unit = {
+    System.err.println(.stripMargin)
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    if (args.length < 3) {
+      System.err.println("Usage: SparkKMeans <file> <k> <convergeDist>")
+      System.exit(1)
+    }
+
+    showWarning()
+
+    val spark = SparkSession
+      .builder
+      .appName("SparkKMeans")
+      .getOrCreate()
+
+    val lines = spark.read.textFile(args(0)).rdd
+    val data = lines.map(parseVector _).cache()
+    val K = args(1).toInt
+    val convergeDist = args(2).toDouble
+
+    val kPoints = data.takeSample(withReplacement = false, K, 42)
+    var tempDist = 1.0
+
+    while(tempDist > convergeDist) {
+      val closest = data.map (p => (closestPoint(p, kPoints), (p, 1)))
+
+      val pointStats = closest.reduceByKey{case ((p1, c1), (p2, c2)) => (p1 + p2, c1 + c2)}
+
+      val newPoints = pointStats.map {pair =>
+        (pair._1, pair._2._1 * (1.0 / pair._2._2))}.collectAsMap()
+
+      tempDist = 0.0
+      for (i <- 0 until K) {
+        tempDist += squaredDistance(kPoints(i), newPoints(i))
+      }
+
+      for (newP <- newPoints) {
+        kPoints(newP._1) = newP._2
+      }
+      println(s"Finished iteration (delta = $tempDist)")
+    }
+
+    println("Final centers:")
+    kPoints.foreach(println)
+    spark.stop()
+  }
+}
